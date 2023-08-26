7640413fd5fce55c065d58dd31258ffea3820b05
Naitik Sharma <126913992+shutter-codes@users.noreply.github.com>
2023-08-26 20:35:56 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
+import { Routes,Route,BrowserRouter } from "react-router-dom";
 import SingleProductPage from "./Pages/SingleProductPage";
+import HomePage from './Pages/HomePage'
 
 export default function App() {
   return (
-  <>
-  {/* <HomePage/> */}
-  <SingleProductPage/>
-  </>
+<BrowserRouter>
+
+<Routes>
+
+<Route path="/" element={<HomePage/>} />
+<Route path="/Product" element={<SingleProductPage/>} />
+
+
+
+ </Routes>
+
+</BrowserRouter>
   );
 }
